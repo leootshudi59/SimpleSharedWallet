df86ee735fc56a98fc4f06a7aa05d3d179bbc4fd
"leootshudi59" <"leootshudi@gmail.com">
2022-04-22 12:51:19 +0200

    feat: Send & withdraw money simple functions

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -4,4 +4,22 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract SimpleWallet {
 
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }  
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not owner");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+        _to.transfer(_amount);
+    }
+    
+    fallback () external payable {
+
+    }
 }
